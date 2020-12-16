8ee62f3cbf27cb7a621fa34f16ea3dd851fc670c
SherryS997 <sherrythomas997@protonmail.com>
2020-12-16 22:39:05 +0530

    Minor Changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,29 +2,52 @@ import 'package:flutter/material.dart';
 import 'main_body.dart';
 import 'header.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:animated_theme_switcher/animated_theme_switcher.dart';
+import 'package:theme_provider/theme_provider.dart';
+import 'package:flutter/scheduler.dart';
 import 'theme.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+bool _hasBeenPressed = true;
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    final isPlatformDark =
-        WidgetsBinding.instance.window.platformBrightness == Brightness.dark;
-    final initTheme = isPlatformDark ? darkTheme : lightTheme;
     return ThemeProvider(
-      initTheme: initTheme,
-      child: Builder(builder: (context) {
-        return MaterialApp(
-          title: 'The Indian Insight',
-          theme: ThemeProvider.of(context),
-          home: MyHomePage(title: 'the indian insight'),
-        );
-      }),
+      saveThemesOnChange: true,
+      loadThemeOnInit: false,
+      onInitCallback: (controller, previouslySavedThemeFuture) async {
+        String savedTheme = await previouslySavedThemeFuture;
+        if (savedTheme != null) {
+          controller.setTheme(savedTheme);
+        } else {
+          Brightness platformBrightness =
+              SchedulerBinding.instance.window.platformBrightness;
+          if (platformBrightness == Brightness.dark) {
+            controller.setTheme('dark');
+            _hasBeenPressed = false;
+          } else {
+            controller.setTheme('light');
+            _hasBeenPressed = true;
+          }
+          controller.forgetSavedTheme();
+        }
+      },
+      themes: <AppTheme>[
+        AppTheme.light(id: 'light'),
+        AppTheme.dark(id: 'dark'),
+      ],
+      child: ThemeConsumer(
+        child: Builder(
+          builder: (themeContext) => MaterialApp(
+            theme: ThemeProvider.themeOf(themeContext).data,
+            title: 'The Indian Insight',
+            home: MyHomePage(title: 'the indian insight'),
+          ),
+        ),
+      ),
     );
   }
 }
@@ -38,55 +61,36 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  bool _hasBeenPressed = false;
   @override
   Widget build(BuildContext context) {
-    return ThemeSwitchingArea(
-      child: Scaffold(
-        // backgroundColor: Colors.yellow[50],
-        appBar: AppBar(
-          leading: ThemeSwitcher(
-            builder: (context) {
-              return IconButton(
-                icon: _hasBeenPressed
-                    ? Icon(
-                        FontAwesomeIcons.sun,
-                        color: Colors.brown[900],
-                      )
-                    : Icon(
-                        FontAwesomeIcons.moon,
-                        color: Colors.brown[900],
-                      ),
-                onPressed: () => {
-                  ThemeSwitcher.of(context).changeTheme(
-                    theme:
-                        ThemeProvider.of(context).brightness == Brightness.light
-                            ? pinkTheme
-                            : darkTheme,
-                    reverseAnimation: false,
-                  ),
-                  setState(
-                    () {
-                      _hasBeenPressed = !_hasBeenPressed;
-                    },
-                  ),
-                },
-                splashRadius: 20,
-                splashColor: Colors.brown[900],
-                autofocus: true,
-              );
-            },
-          ),
-          elevation: 0,
-          centerTitle: true,
-          toolbarHeight: 80,
-          toolbarOpacity: 1,
-          title: Header(widget: widget),
+    var controller = ThemeProvider.controllerOf(context);
+    return Scaffold(
+      // backgroundColor: Colors.yellow[50],
+      appBar: AppBar(
+        leading: IconButton(
+          icon: _hasBeenPressed
+              ? Icon(
+                  FontAwesomeIcons.sun,
+                  // color: Colors.brown[900],
+                )
+              : Icon(
+                  FontAwesomeIcons.moon,
+                  // color: Colors.brown[900],
+                ),
+          onPressed: controller.nextTheme,
+          splashRadius: 20,
+          // splashColor: Colors.brown[900],
+          autofocus: true,
         ),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: MainBody(),
-        ),
+        elevation: 0,
+        centerTitle: true,
+        toolbarHeight: 80,
+        toolbarOpacity: 1,
+        title: Header(widget: widget),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: MainBody(),
       ),
     );
   }
