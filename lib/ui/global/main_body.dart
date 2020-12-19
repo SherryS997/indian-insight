76dc7bafc13aa042bdd24c979cf81b8717258662
SherryS997 <sherrythomas997@protonmail.com>
2020-12-19 20:44:57 +0530

    .

--- lib/ui/global/main_body.dart
+++ lib/ui/global/main_body.dart
@@ -18,9 +18,30 @@ class _MainBodyState extends State<MainBody> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              'jjjameson',
-              // style: AppTheme,
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: FittedBox(
+                child: Container(
+                  // decoration: BoxDecoration(),
+                  child: Column(
+                    children: [
+                      Image(
+                        fit: BoxFit.fitWidth,
+                        height: 300,
+                        width: 500,
+                        image: AssetImage(
+                            'assets/pics/landscape-1844229_1280.png'),
+                      ),
+                      Divider(
+                        height: 20,
+                      ),
+                      Text(
+                        'jjjameson',
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             )
           ],
         ),
